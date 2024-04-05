f32f4f4fc61e18dd1bffcdbe0384133ddf49c72c
naralicecosta <naralicecosta04@gmail.com>
2024-04-05 10:06:14 -0300

    feat: :sparkles: ajustando lógica dos botões de passar página

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,18 +1,39 @@
+
 import {Search, MoreHorizontal, ChevronRight, ChevronLeft, ChevronsLeft, ChevronsRight} from 'lucide-react'
+import dayjs from 'dayjs'
+import 'days/locale/ptBR'
+import relativeTime from 'dayjs/plugin/relativeTime'
 import { IconButton } from './icon-button'
 import { Table } from './table/table'
 import { TableHeader } from './table/table-header'
 import { TableCell } from './table/table-cell'
 import { TableRow } from './table/table-row'
 import { ChangeEvent, useState } from 'react'
+import { attendees } from './data/attendees'
 
+dayjs.extend(relativeTime)
+dayjs.locale('pt-br')
 
 export function AttendeeList(){
     const [search, setSearch] =  useState('')
+    const [page, setPage] = useState(1)
+
+    const totalPages = Math.ceil(attendees.length /10 )
 
     function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
- 
         setSearch(event.target.value)
+    }
+    function goToFirstPage(){
+        setPage(1)
+    }
+    function goToNextPage(){
+        setPage(page + 1)
+    }
+    function goToPreviousPage(){
+        setPage(page - 1)
+    }
+    function goToLastPage(){
+        setPage(totalPages)
     }
 
 
@@ -43,21 +64,22 @@ export function AttendeeList(){
                         </tr>
                     </thead>
                     <tbody>
-                        {Array.from({length: 8}).map((_, i) => {
+                        {attendees.slice((page - 1) * 10, page * 10).map((attendee) => {
                             return(
-                                <TableRow key={i}>
+                                <TableRow key={attendee.id}>
                                 <TableCell >
                                     <input type="checkbox"   className='size-4 bg-black/20 rounded border border-white/10'/>
                                 </TableCell  >
-                                <TableCell >121212</TableCell >
+                                <TableCell >{attendee.id}</TableCell >
                                 <TableCell>
                                     <div className='flex flex-col gap-1'>
-                                        <span className='font-semibold text-white'>Nara</span>
-                                        <span>naralice@</span>
+                                        <span className='font-semibold text-white'>{attendee.name}</span>
+                                        <span>{attendee.email}</span>
                                     </div>
                                 </TableCell >
-                                <TableCell >7 dias atas</TableCell >
-                                <TableCell >3 dias atras</TableCell >
+                                <TableCell >{dayjs().to(attendee.createdAt)}</TableCell >
+                                <TableCell >{dayjs().to(attendee.checkedInAt)}</TableCell >
+
                                 <TableCell >
                                     <IconButton transparent>
                                         <MoreHorizontal className='size-4'/>
@@ -71,28 +93,29 @@ export function AttendeeList(){
                     <tfoot>
                         <tr>
                             <TableCell colSpan={3}>
-                                mostrando 10 de 228 itens 
+                                 mostrando 10 de {attendees.length} itens
                             </TableCell>
 
                             <TableCell className="text-right"colSpan={3}>
                                 <div className='inline-flex items-center gap-8'>
-                                    <span>Página 1 de 23</span>
+                                    <span>Página {page} de {totalPages} </span>
                               
 
                             <div className=' flex gap-1.5'>
-                                <IconButton>
+                                {/* Botão deve estar desabilitado caso a página que o usuário esteja ja seja a primeira */}
+                                <IconButton onClick={goToFirstPage} disabled={page === 1}> 
                                     <ChevronsLeft className='size-4'/>
                                 </IconButton>
-
-                                <IconButton>
+                                {/* So não posso clicar se a página que eu estiver for a página 1 */}
+                                <IconButton onClick={goToPreviousPage} disabled={page === 1}>
                                     <ChevronLeft className='size-4'/>
                                 </IconButton>
-
-                                <IconButton>
+                                {/* Não posso clicar se o número de página que o usuário estiver ja for o número de páginas */}
+                                <IconButton onClick={goToNextPage} disabled={page === totalPages}>
                                     <ChevronRight className='size-4'/>
                                 </IconButton>
-
-                                <IconButton>
+                                 {/* Não posso clicar se o número de página que o usuário estiver ja for o número de páginas */}
+                                <IconButton onClick={goToLastPage} disabled={page === totalPages}>
                                     <ChevronsRight className='size-4'/>
                                 </IconButton>
                             </div>
